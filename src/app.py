7160040271e13b1775cfbbcaed19f7be599fc454
nevooronni <nevooronni@gmail.com>
2018-12-31 00:09:54 +0300

    [crud endpoints] added read, update, delete endpoint for login user and other others

--- src/app.py
+++ src/app.py
@@ -3,6 +3,9 @@ from flask import Flask
 from .config import app_config
 from .models import db, bcrypt
 
+#import user_api blueprint
+from .views.UserView import user_api as user_blueprint
+
 def create_app(env_name):
   """
     create app
@@ -16,6 +19,9 @@ def create_app(env_name):
   #wrap db and bcypt with app and initialize them
   bcrypt.init_app(app)
   db.init_app(app)
+
+  #register user_lueprint
+  app.register_blueprint(user_blueprint, url_prefix='/api/v1/users')
 
   @app.route('/', methods=['GET'])
   def index():
